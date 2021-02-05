d3bd76019c25d67529213a037d95e4b6a1854e72
Maksim996 <12usa34airbornE@gmail.com>
2021-02-05 10:55:53 +0200

    refactor code

--- src/components/TodoListItem/TodoListItem.js
+++ src/components/TodoListItem/TodoListItem.js
@@ -1,24 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './TodoListItem.css'
 
-const TodoListItem = ({ label, important = false }) => {
-  const style = {
-    color : important ? "tomato" : "black"
-  }
+export default class TodoListItem extends Component {
+ render() {
+   const {label, important} = this.props;
 
-  return (
-    <div className="todo-list-item">
-      <span style={style}>{ label }</span>
-      <div className="d-flex align-items-center" role="group" aria-label="Basic example">
-        <button type="button" className="btn btn-outline-danger">
-          <i className="fa fa-trash"></i>
-        </button>
-        <button type="button" className="btn btn-outline-success">
-          <i className="fa fa-exclamation"></i>
-        </button>
-      </div>
-    </div>
-  );
-};
+   const style = {
+     color : important ? "tomato" : "black"
+   };
 
-export default TodoListItem;
+   return (
+     <div className="todo-list-item">
+       <span style={style}>{ label }</span>
+       <div className="d-flex align-items-center" role="group" aria-label="Basic example">
+         <button type="button" className="btn btn-outline-danger">
+           <i className="fa fa-trash"></i>
+         </button>
+         <button type="button" className="btn btn-outline-success">
+           <i className="fa fa-exclamation"></i>
+         </button>
+       </div>
+     </div>
+   );
+ }
+}
--- src/components/TodoStatusFilter/TodoStatusFilter.js
+++ src/components/TodoStatusFilter/TodoStatusFilter.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './TodoStatusFilter.css';
 
-const TodoStatusFilter = () => {
-  return (
-    <div className="btn-group todo-status-filter" role="group" aria-label="Basic example">
-      <button type="button" className="btn btn-primary">All</button>
-      <button type="button" className="btn btn-outline-secondary">Active</button>
-      <button type="button" className="btn btn-outline-secondary">Done</button>
-    </div>
-  )
+export default class TodoStatusFilter extends Component {
+  render() {
+    return (
+      <div className="btn-group todo-status-filter" role="group" aria-label="Basic example">
+        <button type="button" className="btn btn-primary">All</button>
+        <button type="button" className="btn btn-outline-secondary">Active</button>
+        <button type="button" className="btn btn-outline-secondary">Done</button>
+      </div>
+    )
+  }
 }
-
-export default TodoStatusFilter;
