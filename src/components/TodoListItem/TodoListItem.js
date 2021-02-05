8b67e693f1926a4558b7ce58d297cc052df91bee
Maksim996 <12usa34airbornE@gmail.com>
2021-02-05 11:59:07 +0200

    click on label and important

--- src/components/TodoListItem/TodoListItem.js
+++ src/components/TodoListItem/TodoListItem.js
@@ -2,21 +2,37 @@ import React, { Component } from 'react';
 import './TodoListItem.css'
 
 export default class TodoListItem extends Component {
- render() {
-   const {label, important} = this.props;
+ state = {
+   done: false,
+   important: this.props.important
+  };
 
-   const style = {
-     color : important ? "tomato" : "black"
-   };
+ onLabelClick = () => {
+    this.setState(({ done })=>{ return { done: !done }});
+ };
+
+ onImportantClick = () => {
+   this.setState(({important}) => {return { important: !important }})
+ };
+
+ render() {
+   const {label} = this.props;
+   const {done, important} = this.state;
+   const classLabel = [important ? 'text-danger' :'text-primary'];
+
+   if (done) classLabel.push('done');
 
    return (
      <div className="todo-list-item">
-       <span style={style}>{ label }</span>
+       <span className={classLabel.join(' ')} onClick={this.onLabelClick}>{ label }</span>
        <div className="d-flex align-items-center" role="group" aria-label="Basic example">
          <button type="button" className="btn btn-outline-danger">
            <i className="fa fa-trash"></i>
          </button>
-         <button type="button" className="btn btn-outline-success">
+         <button type="button"
+                 className={`btn btn-outline-success ${important ? "active" : ""}`}
+                 onClick={this.onImportantClick}
+         >
            <i className="fa fa-exclamation"></i>
          </button>
        </div>
